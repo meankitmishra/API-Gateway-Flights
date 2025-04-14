bf13c86451cd27a4d05faf236d477eabb09aea5d
meankitmishra <meankitmishra10@gmail.com>
2025-04-14 17:46:36 +0530

    added rate limit and proxy server

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -7,4 +7,6 @@ module.exports = {
 	SALT_ROUNDS: process.env.SALT_ROUNDS,
 	JWT_SECRET: process.env.JWT_SECRET,
 	JWT_EXPIRIY: process.env.JWT_EXPIRIY,
+	FLIGHT_SERVICE: process.env.FLIGHT_SERVICE,
+	BOOKING_SERVICE: process.env.BOOKING_SERVICE,
 };
