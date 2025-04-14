0b22ee2b9a3f6ae5a0a222c8e606ffae7a893e6f
meankitmishra <meankitmishra10@gmail.com>
2025-04-14 15:09:17 +0530

    added jwt expiry error handler

--- src/middlewares/auth-request-middlewares.js
+++ src/middlewares/auth-request-middlewares.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const { errorResponse } = require("../utils/common");
+const { UserService } = require("../services");
 const AppError = require("../utils/errors/app-error");
 
 function validateAuthRequest(req, res, next) {
@@ -22,6 +23,21 @@ function validateAuthRequest(req, res, next) {
 	next();
 }
 
+async function checkAuth(req, res, next) {
+	try {
+		const response = await UserService.isAuthenticated(
+			req.headers["x-access-token"]
+		);
+		if (response) {
+			req.user = response;
+			next();
+		}
+	} catch (error) {
+		return res.status(error.statusCode).json(error);
+	}
+}
+
 module.exports = {
 	validateAuthRequest,
+	checkAuth,
 };
